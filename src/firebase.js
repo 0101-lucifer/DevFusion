052bad4e00b178109a562dc23b2eb558da951f5e
0101-Lucifer <sadiqpathan0304@gmail.com>
2026-04-19 02:34:37 +0530

    Secured Firebase keys in .env file

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,9 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration from the screenshot
 const firebaseConfig = {
-  apiKey: "AIzaSyDhlWJZYYsKquRd9a4BYaqK-yMqQwm5rYw",
-  authDomain: "spwebagency-f7663.firebaseapp.com",
-  projectId: "spwebagency-f7663",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "spwebagency-f7663.firebasestorage.app",
   messagingSenderId: "297021463392",
   appId: "1:297021463392:web:39a1df644006c250ef1c05",
